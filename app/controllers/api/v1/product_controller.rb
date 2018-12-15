d8e42ceb3636291de8345a9a348a44b1847890b2
Avinash kumar <4791avi@gmail.com>
2018-12-15 16:58:32 +0000

    first commit

---
+++ app/controllers/api/v1/product_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::ProductsController < Api::V1::ApplicationController
+  def create
+  end
+end
---
+++ db/migrate/20181215165451_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :sku_id
+      t.text :categories
+      t.text :tags
+      t.integer :price
+      t.text :metadata
+
+      t.timestamps
+    end
+  end
+end

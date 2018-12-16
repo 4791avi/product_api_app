b17c7f516c69a85e8e95f008be44e1a835c7ef22
4791avi <4791avi@gmail.com>
2018-12-16 10:26:21 +0530

    validation in model product and create and update action for api

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,17 +2,33 @@ require 'open-uri'
 class Api::V1::ProductsController < Api::V1::ApplicationController
 	skip_before_action :verify_authenticity_token
   def create
+	sku_id = params[:parameters]["sku_id"]
+	@product = Product.find_by(sku_id: sku_id)
+	if @product
+		update_product = @product.update(name: params[:parameters]["name"],categories: params[:parameters]["categories"],tags: params[:parameters]["tags"],price: params[:parameters]["price"],expire_date: params[:parameters]["expire_date"],metadata: params[:parameters])
+		puts  params[:parameters]["images"]
+		puts image_url = params[:parameters]["images"]
+		puts image_url = image_url[0]["img_path"]
+		downloaded_image = open(image_url)
+		@product.image.purge
+		update_product.image.attach(io: downloaded_image  , filename: "product_image.jpg")
+		if update_product
+			render json: {status: 'SUCCESS', message: 'Product Updated',data: @product},status: :ok
+		else
+			render json: {status: 'ERROR', message: 'Product not saved',data: @product.errors},status: :unprocessable_entity
+		end 
+	else			
 		product = Product.new(name: params[:parameters]["name"],sku_id: params[:parameters]["sku_id"],categories: params[:parameters]["categories"],tags: params[:parameters]["tags"],price: params[:parameters]["price"],expire_date: params[:parameters]["expire_date"],metadata: params[:parameters])
 		puts  params[:parameters]["images"]
 		puts image_url = params[:parameters]["images"]
 		puts image_url = image_url[0]["img_path"]
-		puts image_url.class
 		downloaded_image = open(image_url)
 		product.image.attach(io: downloaded_image  , filename: "mona.jpg")
-	if product.save
-		render json: {status: 'SUCCESS', message: 'Save product',date: product},status: :ok
-	else
-		render json: {status: 'ERROR', message: 'Product not saved',date: product.errors},status: :unprocessable_entity
-	end  	
+		if product.save
+			render json: {status: 'SUCCESS', message: 'Product saved',data: product},status: :ok
+		else
+			render json: {status: 'ERROR', message: 'Product not saved',data: product.errors},status: :unprocessable_entity
+		end 
+	end 	
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,12 @@
 class Product < ApplicationRecord
+# Serializations
 	serialize :category, Array
 	serialize :tags, Array
 	serialize :metadata, JSON
-
-	has_one_attached :image	
+#Associations
+	has_one_attached :image
+#Validation
+	validates :price, presence: true, numericality: { only_integer: true }
+	validates :sku_id, presence: true
+	validates :name, presence: true, format: { with: /\A[a-zA-Z ]+\z/ }
 end
